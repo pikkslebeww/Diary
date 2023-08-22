68c67e26662afb0570ff4c6b036db3095a87a553
pikkslebeww <noviansyahh2006@gmail.com>
2023-08-22 13:18:57 +0700

    week4

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ from datetime import datetime
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
 MONGODB_URI= os.environ.get("MONGODB_URI")
 DB_NAME = os.environ.get("DB_NAME")
+
 client = MongoClient (MONGODB_URI)
 db = client [DB_NAME]
  
